dc9a8d070325cfe66150927504b2245a92340f24
terisumadchat <47727607+terisumadchat@users.noreply.github.com>
2019-03-26 22:54:09 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -7,21 +7,23 @@ using namespace std;
     int main()
     {
     
-    	int ticketPrice;
-    	int ticketSold;
-    	int totalSales;
+    	int box = 250;
+	int sideline = 100;
+	int premium = 50;
+	int general = 25;
+	int a= 5750, b = 28000, c=35750, d=18750; 
+    	int total;
     
-	cout << "Ticket Price: \n";
-	cin >> ticketPrice;
+	cout << "Total sales in box tickets: " << box*a<<"$"<<endl;
 	
-	cout<< "No. of Tickets Sold: \n";
-	cin>> ticketSold;
+	cout<< "Total sales in sideline tickets: "<<sideline*b<<"$"<<endl;
+	cout << "Total sales in premium tickets: "<<premium*c<<"$"<<endl;
+	cout << "Total sales in general admission tickets: "<<d*general<<"$"<<endl;
 	
-	totalSales=ticketPrice*ticketSold;
-	cout<< "--------------------------------------------- \n"; 
-	cout << "TOTAL SALES: "<<totalSales<<"$"<<endl;
-	cout<< "--------------------------------------------- \n";  
+	total=( box*a)+(sideline*b)+(premium*c)+(d*general);
+	cout<< "---------------------------------------------\n"; 
+	cout << "TOTAL SALES IN TICKETS: "<<total<<"$"<<endl;
+	cout<< "---------------------------------------------\n"; 
         getch;
         return 0;
     }
-
